f89e3d390f049f8bae13f40e82b9e63718d7e9f1
Matt Horst <mattahorst@gmail.com>
2024-10-28 14:34:00 -0600

    Updates style

--- PA04/race.c
+++ PA04/race.c
@@ -3,24 +3,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-struct {
+typedef struct {
     int balance[2];
-} Bank = {{100, 100}};      // global variable defined
+} Bank;
 
-void* MakeTransactions() {  // routine for thread execution
+// Define global variables
+Bank bank = {{100, 100}};
+
+/* Routine for thread execution */
+void* make_transactions() {
     int i, j, tmp1, tmp2, rint;
     double dummy;
+
     for (i = 0; i < 100; i++) {
         rint = (rand() % 30) - 15;
-        if (((tmp1 = Bank.balance[0]) + rint) >= 0 &&
-                ((tmp2 = Bank.balance[1]) - rint) >= 0) {
-            Bank.balance[0] = tmp1 + rint;
+
+        if (((tmp1 = bank.balance[0]) + rint) >= 0 &&
+                ((tmp2 = bank.balance[1]) - rint) >= 0) {
+            bank.balance[0] = tmp1 + rint;
             for (j = 0; j < rint * 1000; j++) {
                 dummy = 2.345 * 8.765 / 1.234;
             }  // spend time on purpose
-            Bank.balance[1] = tmp2 - rint;
+            bank.balance[1] = tmp2 - rint;
         }
     }
+
     return NULL;
 }
 int main(int argc, char** argv) {
@@ -28,19 +35,30 @@ int main(int argc, char** argv) {
     void* voidptr = NULL;
     pthread_t tid[2];
     srand(getpid());
-    printf("Init balances A:%d + B:%d ==> %d!\n", Bank.balance[0],
-            Bank.balance[1], Bank.balance[0] + Bank.balance[1]);
-    for (i = 0; i < 2; i++)
-        if (pthread_create(&tid[i], NULL, MakeTransactions, NULL)) {
+
+    // Print initial state
+    printf("Init balances A:%d + b:%d ==> %d!\n", 
+            bank.balance[0], bank.balance[1], bank.balance[0] + bank.balance[1]);
+
+    // Create threads
+    for (i = 0; i < 2; i++) {
+        if (pthread_create(&tid[i], NULL, make_transactions, NULL)) {
             perror("Error in thread creating\n");
             return (1);
         }
-    for (i = 0; i < 2; i++)
+    }
+
+    // Join threads
+    for (i = 0; i < 2; i++) {
         if (pthread_join(tid[i], (void*)&voidptr)) {
             perror("Error in thread joining\n");
             return (1);
         }
-    printf("Let's check the balances A:%d + B:%d ==> %d ?= 200\n",
-            Bank.balance[0], Bank.balance[1], Bank.balance[0] + Bank.balance[1]);
+    }
+
+    // Print results
+    printf("Let's check the balances A:%d + b:%d ==> %d ?= 200\n",
+            bank.balance[0], bank.balance[1], bank.balance[0] + bank.balance[1]);
+
     return 0;
 }
